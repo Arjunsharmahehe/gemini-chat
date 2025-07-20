9b2addcd5585abd7a09d6d563e7665380d54b6af
Arjunsharmahehe <whocaresaboutarjun@gmail.com>
2025-07-20 09:10:55 +0530

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import ModelContextProvider from './context/ModelContext.tsx'
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ModelContextProvider>
       <App />
+      <Analytics />
     </ModelContextProvider>
   </StrictMode>,
 )
